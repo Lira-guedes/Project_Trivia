76d8c4cb996e9aba21576f73f121d2a4333a4b21
Mariana Correa <mpcs270391@gmail.com>
2023-06-27 16:09:06 -0300

    fix: Corrige erro de linter

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 const THREE = 3;
