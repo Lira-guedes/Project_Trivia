001ff823f3ec091a08c18cfb6a60cec38f757627
Henrique Gomes Teixeira <henriquegomes080012@gmail.com>
2023-06-27 16:47:32 -0300

    fix: Requisito 13 - juntei meu estado global que conta os acertos com o código da pasta feedback da mariana

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
-export default class Feedback extends Component {
+const THREE = 3;
+class Feedback extends Component {
+  state = {
+    feedbackMessage: '',
+  };
+
+  componentDidMount() {
+    this.getFeedbackMessage();
+  }
+
+  getFeedbackMessage = () => {
+    const { numberOfRights } = this.props;
+    let feedbackMessage;
+
+    if (numberOfRights < THREE) {
+      feedbackMessage = 'Could be better...';
+    } else {
+      feedbackMessage = 'Well Done!';
+    }
+
+    this.setState({ feedbackMessage }, () => {
+      console.log(numberOfRights);
+    });
+  };
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/');
   };
 
   render() {
+    const { feedbackMessage } = this.state;
     return (
       <>
         <div>
           <Header />
-          <p data-testid="feedback-text">Feedback</p>
+          <p data-testid="feedback-text">{ feedbackMessage }</p>
         </div>
         <div>
           <button
@@ -28,8 +54,15 @@ export default class Feedback extends Component {
   }
 }
 
+const mapStateToProps = ({ player }) => ({
+  numberOfRights: player.correct_question,
+});
+
 Feedback.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
+  numberOfRights: PropTypes.number.isRequired,
 };
+
+export default connect(mapStateToProps)(Feedback);
