29aa22e9ad90dd5326b589a7f5a24871967fd830
Lira Guedes <liracrg@gmail.com>
2023-06-27 19:20:04 -0300

    feat: R 17 - Iniciando requisito

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -5,19 +5,34 @@ import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import App from '../App';
 
 describe('Teste da página de Feedback', () => {
-  it('Verificação dos componentes na tela de Feedback', async () => {
+  it('Verificação do botão play again na tela de Feedback', () => {
     const { history } = renderWithRouterAndRedux(<App />)
     act(() => { history.push('/feedback'); });
 
     const btnPlayAgain = screen.getByRole('button', {name: /play again/i});
-    const btnRanking = screen.getByRole('button', {name: /ranking/i});
-
     expect(btnPlayAgain).toBeInTheDocument();
-    expect(btnRanking).toBeInTheDocument();
 
     userEvent.click(btnPlayAgain);
     expect(history.location.pathname).toBe('/');
+  })
+  it('Verificação do botão ranking na tela de Feedback', () => {
+    const { history } = renderWithRouterAndRedux(<App />)
+    act(() => { history.push('/feedback'); });
+
+    const btnRanking = screen.getByRole('button', {name: /ranking/i});
+    expect(btnRanking).toBeInTheDocument();
+
     userEvent.click(btnRanking);
     expect(history.location.pathname).toBe('/ranking');
+  })
+  it('Verificação dos componentes na tela de Feedback', () => {
+    const { history } = renderWithRouterAndRedux(<App />)
+    act(() => { history.push('/feedback'); });
+    expect(screen.getByTestId('feedback-text')).toBeInTheDocument();
+    // expect(screen.getByTestId('feedback-total-question')).toBeInTheDocument();
+    // expect(screen.getByTestId('feedback-total-score')).toBeInTheDocument();
+    // expect(screen.getByTestId('header-profile-picture')).toBeInTheDocument();
+    // expect(screen.getByTestId('header-player-name')).toBeInTheDocument();
+    // expect(screen.getByTestId('header-score')).toBeInTheDocument();
   })
 })
